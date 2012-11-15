2b2ca027c0f8b4c5cd3438b2ef0e7fb03d1143c3
Mary Campione <mcampione@google.com>
2012-11-15 14:10:30 -0800

    added example for target 4 and cleaned up other samples

--- web/target04/todo-with-delete/todo-with-delete.dart
+++ web/target04/todo-with-delete/todo-with-delete.dart
@@ -2,16 +2,20 @@ import 'dart:html';
 
 InputElement toDoInput;
 UListElement toDoList;
+ButtonElement deleteAll;
 
 void main() {
   toDoInput = query('#to-do-input');
   toDoList = query('#to-do-list');
   toDoInput.on.change.add(addToDoItem);
+  deleteAll = query('#delete-all');
+  deleteAll.on.click.add((e) => toDoList.elements.clear());
 }
 
 void addToDoItem(Event e) {
   var newToDo = new LIElement();
   newToDo.text = toDoInput.value;
+  newToDo.on.click.add((e) => newToDo.remove());
   toDoInput.value = '';
   toDoList.elements.add(newToDo);
 }
