809ed245022483c144a977c9125d0b19ffb9440f
Mary Campione <mcampione@google.com>
2012-12-17 10:32:21 -0800

    create new pages for serving

---
+++ web/target06/shout/web/shout.dart
@@ -0,0 +1,14 @@
+import 'package:web_ui/watcher.dart' as watchers;
+String shoutThis='';
+
+void main() {
+}
+
+String palindrome() {
+  var buffer = new StringBuffer(shoutThis);
+  for (int i = shoutThis.length - 1; i >= 0; i--) {
+    buffer.add(shoutThis[i]);
+  }
+  shoutThis = buffer.toString();
+  return buffer.toString();
+}
