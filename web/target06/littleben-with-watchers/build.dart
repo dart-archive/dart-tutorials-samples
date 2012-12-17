187f2fa775d0459b087338939313fa31e1706c64
Mary Campione <mcampione@google.com>
2012-12-17 10:29:20 -0800

    removed a space

--- web/target06/littleben-with-watchers/build.dart
+++ web/target06/littleben-with-watchers/build.dart
@@ -2,5 +2,5 @@ import 'package:web_ui/component_build.dart';
 import 'dart:io';
 
 void main() {
-  build (new Options().arguments, ['web/littleben.html']);
+  build(new Options().arguments, ['web/littleben.html']);
 }
--- web/target06/shout/build.dart
+++ web/target06/shout/build.dart
@@ -2,5 +2,5 @@ import 'package:web_ui/component_build.dart';
 import 'dart:io';
 
 void main() {
-  build (new Options().arguments, ['web/shout.html']);
+  build(new Options().arguments, ['web/shout.html']);
 }
