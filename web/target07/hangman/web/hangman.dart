a23c96e0f90e5a0ae79f5e2f279f9e205acc6bfb
Mary Campione <mcampione@google.com>
2013-01-25 10:16:04 -0800

    adding source code for new hangman sample

---
+++ web/target07/hangman/web/hangman.dart
@@ -0,0 +1,70 @@
+String answer = 'ambidextrous';
+
+List<String> characters   = answer.splitChars();
+List<String> hyphens      = answer.replaceAll(new RegExp('.'), '-').splitChars();
+List<String> wrongletters = new List();
+List<List> hangmandisplay;
+
+String guessedletter = '';
+bool dead = false;
+bool won = false;
+
+void main() {
+  setupHangmanGrid();
+}
+
+void checkit() {
+  const int maxwrong = 7;
+  if (characters.contains(guessedletter)) {  // guess is correct
+    revealletters();
+    if (!hyphens.contains('-')) {
+      won = true;
+    }
+  } else {                                   // guess is wrong
+    revealnewbodypart();                                  
+    wrongletters.add(guessedletter);
+    if (wrongletters.length == maxwrong) {
+      dead = true;
+    }
+  }
+  guessedletter = '';
+}
+
+void revealletters() {
+  for (int i = 0; i < characters.length; i++) {
+    if (characters[i] == guessedletter) {
+      hyphens[i] = guessedletter;
+    }
+  }
+}
+
+void revealnewbodypart() {
+  // triplets: row, col, character for body part
+  var deadmanparts = [ [1,4,'0'],   /* head */
+                       [2,3,'\/'],  /* left arm */
+                       [2,4,'\|'],  /* upper body */
+                       [2,5, '\\'], /* right arm */
+                       [3,4, '\|'], /* lower body */
+                       [4,3, '\/'], /* left leg */
+                       [4,5,'\\']   /* right leg */
+                     ];
+
+  var row = deadmanparts[wrongletters.length][0];
+  var col = deadmanparts[wrongletters.length][1];
+  hangmandisplay[row][col] = deadmanparts[wrongletters.length][2];
+}
+
+void setupHangmanGrid()
+{
+  var gallows = [ '+---+  ',
+                  '\|      ',
+                  '\|      ',
+                  '\|      ',
+                  '\|      ' ];
+  hangmandisplay = new List(gallows.length);
+  
+  for (int i = 0; i < gallows.length; i++) {
+    List<String> row = gallows[i].splitChars();
+    hangmandisplay[i] = row;
+  }
+}
