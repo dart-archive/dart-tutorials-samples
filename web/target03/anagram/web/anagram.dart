2aa184867f96b7b4c468ee30b3ccd6aba51fc8dd
Mary Campione <mcampione@google.com>
2013-02-13 08:41:50 -0800

    updated target03 samples to M3

--- web/target03/anagram/web/anagram.dart
+++ web/target03/anagram/web/anagram.dart
@@ -12,11 +12,12 @@ ButtonElement clearButton;
 Element value;
 int wordvalue = 0;
 
-
-Map scrabbleValues = { 'a':1, 'e':1, 'i':1, 'l':1, 'n':1, 'o':1, 'r':1, 's':1, 't':1, 'u':1,
-                      'd':2, 'g':2, 'b':3, 'c':3, 'm':3, 'p':3,
-                      'f':4, 'h':4, 'v':4, 'w':4, 'y':4, 'k':5, 'j':8, 'x':8, 'q':10, 'z':10,
-                      '*':0 };
+Map scrabbleValues = { 'a':1, 'e':1, 'i':1, 'l':1, 'n':1,
+                       'o':1, 'r':1, 's':1, 't':1, 'u':1,
+                       'd':2, 'g':2, 'b':3, 'c':3, 'm':3,
+                       'p':3, 'f':4, 'h':4, 'v':4, 'w':4,
+                       'y':4, 'k':5, 'j':8, 'x':8, 'q':10,
+                       'z':10, '*':0 };
 
 void main() {
   letterpile = query("#letterpile");
@@ -24,27 +25,22 @@ void main() {
   value = query("#value");
   
   clearButton = query("#clearButton");
-  clearButton.on.click.add(newletters);
+  clearButton.onClick.listen(newletters);
   
   generateNewLetters();
 }
 
-void moveToWord(Event e) {
+void moveLetter(Event e) {
   Element letter = e.target;
-  result.children.add(letter);
-  wordvalue += scrabbleValues[letter.text];
-  value.text = "$wordvalue";
-  letter.on.click.remove(moveToWord);
-  letter.on.click.add(moveToLetters);
-}
-
-void moveToLetters(Event e) {
-  Element letter = e.target;
-  letterpile.children.add(letter);
-  wordvalue -= scrabbleValues[letter.text];
-  value.text = "$wordvalue";
-  letter.on.click.remove(moveToLetters);
-  letter.on.click.add(moveToWord);
+  if (letter.parent == letterpile) {
+    result.children.add(letter);
+    wordvalue += scrabbleValues[letter.text];
+    value.text = "$wordvalue";
+  } else {
+    letterpile.children.add(letter);
+    wordvalue -= scrabbleValues[letter.text];
+    value.text = "$wordvalue";
+  }
 }
 
 void newletters(Event e) {
@@ -63,7 +59,7 @@ generateNewLetters() {
       // should remove the letter from scrabbleLetters to keep the ratio correct
     buttons.add(new ButtonElement());
     buttons[i].classes.add("letter");
-    buttons[i].on.click.add(moveToWord);
+    buttons[i].onClick.listen(moveLetter);
     buttons[i].text = scrabbleLetters[letterIndex];
     letterpile.children.add(buttons[i]);
   }
