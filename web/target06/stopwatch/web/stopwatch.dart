22e3da6a4b1e5f54743b277526af5469dda8ef59
thulka <thulka@yahoo.com>
2013-03-29 17:46:42 +0100

    Adopted Timer Api

--- web/target06/stopwatch/web/stopwatch.dart
+++ web/target06/stopwatch/web/stopwatch.dart
@@ -20,7 +20,7 @@ void main() {
 void startwatch() {
   mywatch.start();
   var oneSecond = new Duration(seconds:1);
-  var timer = new Timer.repeating(oneSecond, updateTime);
+  var timer = new Timer.periodic(oneSecond, updateTime);
   startButton.disabled = true;
   stopButton.disabled = false;
   resetButton.disabled = true;
