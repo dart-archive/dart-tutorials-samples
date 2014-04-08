4490b10db11593730b9b3a745c450959d2d64ec0
Mary Campione <mcampione@google.com>
2014-04-08 09:26:08 -0700

    tweak the code

--- bin/httpserver/hello_world_server.dart
+++ bin/httpserver/hello_world_server.dart
@@ -10,7 +10,7 @@ import 'dart:io';
 main() {
   HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4040)
       .then((HttpServer server) {
-    print('listening on localhost, port 4040');
+    print('listening on localhost, port ${server.port}');
     server.listen((HttpRequest request) {
       request.response.write('Hello, world!');
       request.response.close();
