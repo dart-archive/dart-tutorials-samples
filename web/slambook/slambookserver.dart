2ba7cc9fcc9fa996459c625dc69cdfa025cdb76b
marycampione <mcampione@google.com>
2014-04-08 14:58:01 -0700

    Update slambookserver.dart

--- web/slambook/slambookserver.dart
+++ web/slambook/slambookserver.dart
@@ -60,7 +60,7 @@ void handlePost(HttpRequest req) {
  * and http://enable-cors.org/server.html
  */
 void addCorsHeaders(HttpResponse res) {
-  res.headers.add('Access-Control-Allow-Origin', '*, ');
+  res.headers.add('Access-Control-Allow-Origin', '*');
   res.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 }
