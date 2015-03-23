bbe0c6e8800a165465368235a088f50028761618
Kathy Walrath <kathyw@google.com>
2015-03-23 13:08:55 -0700

    streams/ code
    added await-for versions of some code
    changed " to '

--- streams/bin/feet_wet_streams.dart
+++ streams/bin/feet_wet_streams.dart
@@ -2,8 +2,6 @@
 import 'dart:async';
 // END(consuming_a_stream)
 
-//XXX: Change " to ' in strings?
-
 import 'dart:convert';
 
 // BEGIN(reading_a_file)
@@ -17,6 +15,7 @@ main() {
   // END(reading_a_file)
 
   singleStream();
+  singleStreamAsync();
   streamProperties();
   broadcast();
   streamSubsetsOfData();
@@ -25,17 +24,19 @@ main() {
 
   singleWhere();
   singleError();
-  singleErrorWithCatch();
-
+  singleErrorWithCatchError();
+  singleErrorWithCatchAsync();
+  
   streamSubscription_handlersOnSubscription();
   streamSubscription_handlerFunctionArgs();
 
   unsubscribing();
   generic();
   readingAFile();
+  readingAFileAsync();
 }
 
-singleStream() {
+void singleStream() {
   // BEGIN(consuming_a_stream)
   // BEGIN(simple_stream_code)
   var data = [1, 2, 3, 4, 5]; // some sample data
@@ -45,26 +46,40 @@ singleStream() {
   // subscribe to the streams events
   stream.listen((value) {
     //
-    print("Received: $value"); // onData handler
+    print('Received: $value'); // onData handler
   }); //
 }
 // END(consuming_a_stream)
+
+Future singleStreamAsync() async {
+  // BEGIN(consuming_a_stream_async)
+  // BEGIN(simple_stream_code_async)
+  var data = [1, 2, 3, 4, 5]; // some sample data
+  var stream = new Stream.fromIterable(data); // create the stream
+  // END(simple_stream_code_async)
+
+  // wait for all events in the stream
+  await for (var value in stream) {
+    print('Received: $value'); // onData handler
+  }
+}
+// END(consuming_a_stream_async)
 
 Future streamProperties() async {
   var stream;
 
   // BEGIN(stream_properties)
   stream = new Stream.fromIterable([1, 2, 3, 4, 5]);
-  print("stream.first: ${await stream.first}"); // 1
+  print('stream.first: ${await stream.first}'); // 1
 
   stream = new Stream.fromIterable([1, 2, 3, 4, 5]);
-  print("stream.last: ${await stream.last}"); // 5
+  print('stream.last: ${await stream.last}'); // 5
 
   stream = new Stream.fromIterable([1, 2, 3, 4, 5]);
-  print("stream.isEmpty: ${await stream.isEmpty}"); // false
+  print('stream.isEmpty: ${await stream.isEmpty}'); // false
 
   stream = new Stream.fromIterable([1, 2, 3, 4, 5]);
-  print("stream.length: ${await stream.length}"); // 5
+  print('stream.length: ${await stream.length}'); // 5
   // END(stream_properties)
 }
 
@@ -75,12 +90,12 @@ Future broadcast() async {
   var broadcastStream = stream.asBroadcastStream();
 
   broadcastStream
-      .listen((value) => print("stream.listen: $value")); //XXX
+      .listen((value) => print('stream.listen: $value')); //XXX
 
-  print("stream.first: ${await broadcastStream.first}"); // 1
-  print("stream.last: ${await broadcastStream.last}"); // 5
-  print("stream.isEmpty: ${await broadcastStream.isEmpty}"); // false
-  print("stream.length: ${broadcastStream.length}"); // 5
+  print('stream.first: ${await broadcastStream.first}'); // 1
+  print('stream.last: ${await broadcastStream.last}'); // 5
+  print('stream.isEmpty: ${await broadcastStream.isEmpty}'); // false
+  print('stream.length: ${broadcastStream.length}'); // 5
   // END(as_broadcast_stream)
 }
 
@@ -96,24 +111,24 @@ streamSubsetsOfData() {
 
   // BEGIN(stream_subsets)
   broadcastStream.where((value) => value % 2 == 0) // divisible by 2
-      .listen((value) => print("where: $value")); // where: 2
+      .listen((value) => print('where: $value')); // where: 2
   // where: 4
 
   broadcastStream.take(3) // takes only the first three elements
-      .listen((value) => print("take: $value")); // take: 1
+      .listen((value) => print('take: $value')); // take: 1
   // take: 2
   // take: 3
 
   broadcastStream.skip(3) // skips the first three elements
-      .listen((value) => print("skip: $value")); // skip: 4
+      .listen((value) => print('skip: $value')); // skip: 4
   // skip: 5
 
   broadcastStream.takeWhile((value) => value < 3) // take while true
-      .listen((value) => print("takeWhile: $value")); // takeWhile: 1
+      .listen((value) => print('takeWhile: $value')); // takeWhile: 1
   // takeWhile: 2
 
   broadcastStream.skipWhile((value) => value < 3) // skip while true
-      .listen((value) => print("skipWhile: $value")); // skipWhile: 3
+      .listen((value) => print('skipWhile: $value')); // skipWhile: 3
   // skipWhile: 4
   // skipWhile: 5
   // END(stream_subsets)
@@ -128,14 +143,14 @@ transformingStream() {
   var transformer = new StreamTransformer.fromHandlers(
       handleData: (value, sink) {
     // create two new values from the original value
-    sink.add("Message: $value");
-    sink.add("Body: $value");
+    sink.add('Message: $value');
+    sink.add('Body: $value');
   });
 
   // transform the stream and listen to its output
   stream
       .transform(transformer)
-      .listen((value) => print("listen: $value"));
+      .listen((value) => print('listen: $value'));
   // END(stream_transformer)
 }
 
@@ -153,17 +168,17 @@ Future validatingStream() async {
   var result;
 
   result = await broadcastStream.any((value) => value < 5);
-  print("Any less than 5?: $result"); // true
+  print('Any less than 5?: $result'); // true
 
   result = await broadcastStream.every((value) => value < 5);
-  print("All less than 5?: $result"); // false
+  print('All less than 5?: $result'); // false
 
   result = await broadcastStream.contains(4);
-  print("Contains 4?: $result"); // true
+  print('Contains 4?: $result'); // true
   // END(validating_stream_data)
 }
 
-singleWhere() {
+singleWhere() async {
   var data = [1, 2, 3, 4, 5];
   var stream = new Stream.fromIterable(data);
   // get the stream as a broadcast stream
@@ -173,8 +188,8 @@ singleWhere() {
   var result;
 
   // Only one value is less than 2
-  result = broadcastStream.singleWhere((value) => value < 2);
-  print("single value: $result"); // single value: 1
+  result = await broadcastStream.singleWhere((value) => value < 2);
+  print('single value: $result'); // single value: 1
   // END(single_where)
 }
 
@@ -190,8 +205,8 @@ singleError() {
     // BEGIN(failure_using_single)
     try {
       // will fail: the stream has more than one value
-      result = broadcastStream.single;
-      print("single value: $result");
+      result = await broadcastStream.single;
+      print('single value: $result');
       return result;
     } catch (e) {
       return e;
@@ -199,10 +214,10 @@ singleError() {
     // END(failure_using_single)
   }
 
-  inner().catchError((err) => print("Expected Error: $err"));
+  inner().catchError((err) => print('Expected Error: $err'));
 }
 
-singleErrorWithCatch() {
+singleErrorWithCatchError() {
   var data = [1, 2, 3, 4, 5];
   var stream = new Stream.fromIterable(data);
   // get the stream as a broadcast stream
@@ -210,9 +225,9 @@ singleErrorWithCatch() {
 
   // BEGIN(catch_error)
   broadcastStream.single // will fail - there is more than one value in the stream
-      .then((value) => print("single value: $value"))
+      .then((value) => print('single value: $value'))
       .catchError((err) => print(
-          "Expected Error: $err")); // catch any error in the then()
+          'Expected Error: $err')); // catch any error in the then()
   // output: Expected Error: Bad State: More than one element
   // END(catch_error)
 }
@@ -226,15 +241,14 @@ Future singleErrorWithCatchAsync() async {
   // BEGIN(catch_error_async)
   try {
     // single will fail: stream has > 1 value
-    print("single value: ${await broadcastStream.single}");
+    print('single value: ${await broadcastStream.single}');
   } catch (e) {
-    print("Expected error: $e"); // catch any error
+    print('Expected error: $e'); // catch any error
     // output: Expected error: Bad State: Too many elements
   }
   // END(catch_error_async)
 }
 
-// XXX: Can I convert this to async*?
 streamSubscription_handlersOnSubscription() {
   var data = [1, 2, 3, 4, 5];
   var stream = new Stream.fromIterable(data);
@@ -242,9 +256,9 @@ streamSubscription_handlersOnSubscription() {
   // BEGIN(subscription_handler_methods)
   // setup the handlers through the subscription's handler methods
   var subscription = stream.listen(null);
-  subscription.onData((value) => print("listen: $value"));
-  subscription.onError((err) => print("error: $err"));
-  subscription.onDone(() => print("done"));
+  subscription.onData((value) => print('listen: $value'));
+  subscription.onError((err) => print('error: $err'));
+  subscription.onDone(() => print('done'));
   // END(subscription_handler_methods)
 }
 
@@ -254,9 +268,9 @@ streamSubscription_handlerFunctionArgs() {
 
   // BEGIN(arguments_to_listen)
   // setup the handlers as arguments to the listen() function
-  var subscription = stream.listen((value) => print("listen: $value"),
-      onError: (err) => print("error: $err"),
-      onDone: () => print("done"));
+  var subscription = stream.listen((value) => print('listen: $value'),
+      onError: (err) => print('error: $err'),
+      onDone: () => print('done'));
   // END(arguments_to_listen)
 }
 
@@ -267,41 +281,60 @@ unsubscribing() {
   // BEGIN(cancelling_a_stream)
   var subscription = stream.listen(null);
   subscription.onData((value) {
-    print("listen: $value");
+    print('listen: $value');
     if (value == 2) subscription.cancel(); // cancel the subscription
   });
-  subscription.onError((err) => print("error: $err"));
-  subscription.onDone(() => print("done"));
+  subscription.onError((err) => print('error: $err'));
+  subscription.onDone(() => print('done'));
   // END(cancelling_a_stream)
 }
 
 generic() {
   // BEGIN(stream_generics)
   var data = [1, 2, 3, 4, 5]; // ints, valid
-  // var data = ["1","2","3","4","5"]; // strings, not valid
+  // var data = ['1','2','3','4','5']; // strings, not valid
   var stream = new Stream<int>.fromIterable(data); // Stream<int>
   stream.listen((value) {
     // value must be an int
-    print("listen: $value");
+    print('listen: $value');
   });
   // END(stream_generics)
 }
 
-readingAFile() {
-  // read this script file
-  //var options = new Options();
-  var this_file = Platform.script;
-  //var thisFilePath = options.script;
+void readingAFile() {
+  // Read this script file
+  var thisFile = Platform.script;
 
-  File file = new File(this_file.toFilePath());
+  File file = new File(thisFile.toFilePath());
   // BEGIN(string_decoder)
   // BEGIN(reading_a_file)
   file
       .openRead()
       .transform(UTF8.decoder)
       .listen((String data) => print(data), // output the data
-          onError: (error) => print("Error, could not open file"),
-          onDone: () => print("Finished reading data"));
+          onError: (error) => print('Error, could not open file'),
+          onDone: () => print('Finished reading a file')
+          );
+
   // END(string_decoder)
 }
-// END(reading_a_file)
+
+Future readingAFileAsync() async {
+  // Read this script file
+  var thisFile = Platform.script;
+
+  File file = new File(thisFile.toFilePath());
+  // BEGIN(string_decoder_async)
+  // BEGIN(reading_a_file_async)
+  try {
+    await for (var data in file.openRead().transform(UTF8.decoder)) {
+      print('ReadingAFileAsync:');
+      print(data);
+    } 
+    print('Finished reading a file (with await for)');
+  } catch (e) {
+      print('Error, could not open file');
+  }
+  // END(string_decoder_async)
+}
+// END(reading_a_file_async)
