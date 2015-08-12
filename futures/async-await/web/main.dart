343cab63f2cc61b4b30b93e4b5b60b5c00b43d6d
Sharon Zakhour <shaza@google.com>
2015-08-12 15:08:47 -0700

    Updating the futures tutorial to use DartPad.

---
+++ futures/async-await/web/main.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'dart:async';
+
+Future printDailyNewsDigest() async {
+  String path =
+      'https://www.dartlang.org/samples-files/dailyNewsDigest.txt';
+  String content = await HttpRequest.getString(path);
+  print(content);
+}
+
+main() {
+  printDailyNewsDigest();
+  printWinningLotteryNumbers();
+  printWeatherForecast();
+  printBaseballScore();
+}
+
+printWinningLotteryNumbers() {
+  print('Winning lotto numbers: [23, 63, 87, 26, 2]');
+}
+
+printWeatherForecast() {
+  print('Tomorrow\'s forecast: 70F, sunny.');
+}
+
+printBaseballScore() {
+  print('Baseball score: Red Sox 10, Yankees 0');
+}
--- futures/futures-api/web/main.dart
+++ futures/futures-api/web/main.dart
@@ -1,9 +1,14 @@
-import 'dart:io';
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
 import 'dart:async';
 
 void printDailyNewsDigest() {
-  File file = new File("dailyNewsDigest.txt");
-  Future future = file.readAsString();
+  String path =
+      'https://www.dartlang.org/samples-files/dailyNewsDigest.txt';
+  Future future = HttpRequest.getString(path);
   future.then((content) {
     print(content);
   });
