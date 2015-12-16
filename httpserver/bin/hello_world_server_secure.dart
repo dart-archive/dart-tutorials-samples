86c7baf872dce59b3faa17c0e2fc444e7eb707c8
William Hesse <whesse@google.com>
2015-12-16 14:56:29 +0100

    Update https server example for Dart 1.13.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/1409073011 .

--- httpserver/bin/hello_world_server_secure.dart
+++ httpserver/bin/hello_world_server_secure.dart
@@ -4,18 +4,24 @@
 
 // Replies "Hello, world!" to all requests.
 // Use the HTTPS URL https://localhost:4047 in your browser.
-// You might get a warning about a potential security risk.
+// This sample requires Dart version 1.13 or later.
+// You might get a warning about a potential security risk, because the
+// server certificate is signed by a test authority.  You can add the
+// server_authority.pem certificate to your test browser as a trusted
+// authority to avoid this message, but that may make that browser less secure.
 
 import 'dart:io';
 
 main() async {
-  var testPkcertDatabase =
-      Platform.script.resolve('pkcert').toFilePath();
-  SecureSocket.initialize(
-      database: testPkcertDatabase, password: 'dartdart');
+  var certificateChain =
+      Platform.script.resolve('server_chain.pem').toFilePath();
+  var serverKey =
+      Platform.script.resolve('server_key.pem').toFilePath();
+  var serverContext = new SecurityContext();
+  serverContext.useCertificateChain(certificateChain);
+  serverContext.usePrivateKey(serverKey, password: 'dartdart');
 
-  var requests = await HttpServer.bindSecure('localhost', 4047,
-      certificateName: 'localhost_cert');
+  var requests = await HttpServer.bindSecure('localhost', 4047, serverContext);
   print('listening');
   await for (HttpRequest request in requests) {
     request.response..write('Hello, world!')
