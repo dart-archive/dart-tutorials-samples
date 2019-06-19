909c92e1a5a60fbee96bfce009dbe93d27bddebf
Brett Morgan <brett.morgan@gmail.com>
2019-06-19 15:00:16 +1000

    streams

--- streams/read_file/bin/main.dart
+++ streams/read_file/bin/main.dart
@@ -7,10 +7,15 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 
-Future main(args) async {
+Future main(List<String> args) async {
+  if (args.isEmpty) {
+    print("Requires a filepath as the first argument");
+    return;
+  }
+
   var file = new File(args[0]);
   var lines =
-      file.openRead().transform(UTF8.decoder).transform(const LineSplitter());
+      file.openRead().transform(utf8.decoder).transform(const LineSplitter());
   await for (var line in lines) {
     if (!line.startsWith('#')) {
       print(line);
